326de433f46ee9567072bd18abb123d1661d2f56
João <joaovictorvha@gmail.com>
2019-10-01 15:02:27 -0400

    generator

--- closures/generator.go
+++ closures/generator.go
@@ -1,20 +1,38 @@
 // Faça um programa que utilize closures para criar uma
 // função que gera (generator) os múltiplos de um número.
 package main
-import "fmt"
 
-func main() {
-	gerar := generator(2)
-	fmt.Println(gerar())
-	fmt.Println(gerar())
-	fmt.Println(gerar())
-	fmt.Println(gerar())
-}
+import (
+	"fmt"
+	"math/rand"
+)
 
-func generator(num int) func() int {
-	n := num
+func generator() func() int {
+	n := 0
+	var lista [60]int
+	
+	i := 0
+    for i < 60 {
+		lista[i] = i + 1
+        i = i + 1
+	}
+	
+	rand.Shuffle(len(lista), func(i, j int) { lista[i], lista[j] = lista[j], lista[i] })
+
 	return func() int {
-		n = n + n
-		return n
+		n++
+		return lista[n]
 	}
 }
+
+func main() {
+	sortear := generator()
+	fmt.Println(sortear())
+	fmt.Println(sortear())
+	fmt.Println(sortear())
+	fmt.Println(sortear())
+	fmt.Println(sortear())
+	fmt.Println(sortear())
+}
+
+
